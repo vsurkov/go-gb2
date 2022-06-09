69ec4807c4b25f50ee7b74e1e3e724ed2ff2e6bf
vsurkov <surkovv@gmail.com>
2022-06-09 22:32:17 +0300

    remove misscommit

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/vsurkov/go-gb2
-
-go 1.18
-
-require github.com/vsurkov/lgr v0.0.2
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	lgr "github.com/vsurkov/lgr"
-)
-
-func main() {
-	lgr.Logger("hello world")
-}
