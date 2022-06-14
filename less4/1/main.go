9698acf8cc105dbc2079480884b6f85ceeafc4fd
vsurkov <surkovv@gmail.com>
2022-06-14 23:12:37 +0300

    rewrite from Mutex to atomic and refactor code

--- less4/1/main.go
+++ less4/1/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/vsurkov/lgr/v2"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -11,24 +12,21 @@ import (
 //Дождаться завершения всех горутин и убедиться, что при каждом запуске программы итоговое число равно 1000.
 func main() {
 	const (
-		count    = 1000
+		count    = 100000
 		buffSize = 10
 	)
 
 	var wg sync.WaitGroup
-	var workers = make(chan struct{}, count)
-	var result = make(chan int)
-	var sum int
+	var workers = make(chan struct{}, buffSize)
+	var result = make(chan int64)
+	var sum int64
 
 	lgr.Logger(fmt.Sprintf("init start with buffSize=%v, count=%v", buffSize, count))
 
 	// горутина читает значения из канала
-	var mu sync.Mutex
 	go func() {
 		for val := range result {
-			mu.Lock()
-			sum += val
-			mu.Unlock()
+			atomic.AddInt64(&sum, val)
 		}
 	}()
 
@@ -38,15 +36,14 @@ func main() {
 		wg.Add(1)
 		go func() {
 			defer func() {
+				time.Sleep(time.Millisecond)
 				wg.Done()
-				<-workers
 			}()
 			result <- 1
+			<-workers
 		}()
 	}
 
 	wg.Wait()
-	// mutex, waitingGroup, буферизированные каналы и все равно без слипа нет 100% гарантии что будет выведено верно
-	time.Sleep(200 * time.Millisecond)
 	lgr.Logger(fmt.Sprintf("result: %d", sum))
 }
