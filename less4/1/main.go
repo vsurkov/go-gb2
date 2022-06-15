2349eb0f7af0f84ee535c2f9738bd4f5d9a9ca98
vsurkov <surkovv@gmail.com>
2022-06-15 21:50:58 +0300

    fix listeners problem without hacks, thanks to Boris

--- less4/1/main.go
+++ less4/1/main.go
@@ -2,33 +2,37 @@ package main
 
 import (
 	"fmt"
-	"github.com/vsurkov/lgr/v2"
-	"runtime"
 	"sync"
 	"sync/atomic"
+
+	"github.com/vsurkov/lgr/v2"
 )
 
 //С помощью пула воркеров написать программу, которая запускает 1000 горутин, каждая из которых увеличивает число на 1.
 //Дождаться завершения всех горутин и убедиться, что при каждом запуске программы итоговое число равно 1000.
 func main() {
 	const (
-		count    = 100000
+		count    = 10000
 		buffSize = 10
 	)
 
-	var wg sync.WaitGroup
 	var workers = make(chan struct{}, buffSize)
 	var result = make(chan int64)
 	var sum int64
 
 	// горутина читает значения из канала
+	var wgl sync.WaitGroup
+	wgl.Add(1)
+
 	go func() {
+		defer wgl.Done()
 		for val := range result {
 			atomic.AddInt64(&sum, val)
 		}
 	}()
 
 	// горутины пишут значения в канал и добавляются в WaitGroup
+	var wg sync.WaitGroup
 	for i := 0; i < count; i++ {
 		workers <- struct{}{}
 		wg.Add(1)
@@ -38,10 +42,10 @@ func main() {
 			}()
 			result <- 1
 			<-workers
-			// использование runtime.Gosched() дало возможность корректно работать без костылей (слипа) перед wg.Wait()
-			runtime.Gosched()
 		}()
 	}
 	wg.Wait()
+	close(result)
+	wgl.Wait()
 	lgr.Logger(fmt.Sprintf("result: %d", sum))
 }
