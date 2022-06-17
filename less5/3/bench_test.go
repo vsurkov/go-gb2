c8af1bc286c78bb5dfcce0c24ccf0981ff313483
vsurkov <surkovv@gmail.com>
2022-06-17 19:15:36 +0300

    refactoring variables

--- less5/3/bench_test.go
+++ less5/3/bench_test.go
@@ -5,33 +5,35 @@ import (
 	"testing"
 )
 
+const scope = 1000
+
 //Протестируйте производительность операций чтения и записи на множестве
 //действительных чисел, безопасность которого обеспечивается sync.Mutex и sync.RWMutex для разных вариантов
 //использования:
 
 //10% запись, 90% чтение;
 func BenchmarkSetUsecaseOne(b *testing.B) {
-	muxSet(b, 10, 1000)
+	muxSet(b, 10, scope)
 }
 
 func BenchmarkRWSetUsecaseOne(b *testing.B) {
-	rwMuxSet(b, 10, 1000)
+	rwMuxSet(b, 10, scope)
 }
 
 ////50% запись, 50% чтение;
 func BenchmarkSetUsecaseTwo(b *testing.B) {
-	muxSet(b, 50, 1000)
+	muxSet(b, 50, scope)
 }
 func BenchmarkRWSetUsecaseTwo(b *testing.B) {
-	rwMuxSet(b, 50, 1000)
+	rwMuxSet(b, 50, scope)
 }
 
 ////90% запись, 10% чтение
 func BenchmarkSetUsecaseThree(b *testing.B) {
-	muxSet(b, 90, 1000)
+	muxSet(b, 90, scope)
 }
 func BenchmarkRWSetUsecaseThree(b *testing.B) {
-	rwMuxSet(b, 90, 1000)
+	rwMuxSet(b, 90, scope)
 }
 
 func muxSet(b *testing.B, writePercent float32, scope float32) {
