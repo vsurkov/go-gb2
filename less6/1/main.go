b35690e79b73c5746c21defa33f2d1a707d266b5
vsurkov <surkovv@gmail.com>
2022-06-19 00:47:30 +0300

    Добавлен код и тесты для 1 задания

---
+++ less6/1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+//1. Написать программу, которая использует мьютекс для безопасного доступа к данным из нескольких потоков.
+//Выполните трассировку программы
+
+func main() {
+	var count = 10000
+	var listeners = runtime.NumCPU() * 8
+
+	fmt.Print(serve(count, listeners))
+}
